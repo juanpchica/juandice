e8492f65541e7e7af7bece6a6a8f2e4d80699f8b
juanpchcia <juanpchica@hotmail.com>
2019-02-09 10:35:57 -0500

    Ejemplo de promesas

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const POKEMON_URL = 'pokemon/:nombre';
 
 //Hago request al api con jquery
 //crossdomain
-
+/*
 saludoPokemon = data => {
 	console.log("Hola "+ data.name+" Tienes de experiencia:"+data.base_experience);
 }
@@ -15,3 +15,43 @@ function getPokemon(nombre){
 getPokemon('pikachu');
 getPokemon('bulbasaur');
 getPokemon('charmander');
+*/
+
+///Promesas
+// .then(val => ...) -> resuelta correctamente
+// .catch(val => ...) -> error de resolucion
+
+//Es un objeto de js, siempre le paso 
+//new Promise(function(resolve,reject){
+
+//}).then().catch()
+
+
+function saludarPokemon(nombre){
+
+	obtenerPokemon(nombre).then(function(pokemon){
+		//Si el pokemon es obtenido correctamente, saludo
+		console.log("Hola eres el pokemon "+pokemon.name+" y tu experiencia es de: "+pokemon.base_experience);
+	})
+	.catch(onError)
+}
+
+function obtenerPokemon(nombre){
+	return new Promise(function(resolve,reject){
+		//Traigo los pokemones mediante httprequest
+		$
+			//Si es correcta la consulta y exitosa, resuelvo la promesa
+			.get(API_URL + POKEMON_URL.replace(':nombre', nombre),data => resolve(data))
+
+			//Si falla la consulta con .get, reject la promesa
+			.fail(function(){reject(nombre)})
+	})
+}
+
+function onError(nombre){
+	console.log("Sucedio un error al obtener el personaje: "+nombre);
+}
+
+//Ejecuto la funcion que tiene la promesa
+saludarPokemon("pikachuh")
+saludarPokemon("pikachu")
