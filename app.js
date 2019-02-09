a5b4773b8eab83a392e416e21a2b6a4b3a70119f
juanpchcia <juanpchica@hotmail.com>
2019-02-09 15:32:24 -0500

    cambiando contextos

--- app.js
+++ app.js
@@ -64,3 +64,35 @@ var promesas = nombresPokemones.map(function(nombre) {
 })
 
 //Para usar await debemos usar async en la funcion 
+
+
+
+//Cambiando contextos
+
+const lis = {
+    nombre: 'Lissette',
+    apellido: 'Garzon'
+}
+
+const anton = {
+    nombre: 'Antonio',
+    apellido: 'Cortes'
+}
+
+function saludar(saludo = 'Hola'){
+   console.log(`${saludo}, mi nombre es ${this.nombre}`)
+}
+
+// saludar(lis)  //esto da Hola, mi nombre es undefined
+
+// const saludoLis = saludar.bind(lis) //no se ejecuta saludar hasta que llame a saludoLis que la referncia
+
+const saludando = (persona,saludo) => saludar.bind(persona,saludo)
+
+saludando(lis)()
+saludando(anton, 'Hola che')()
+setTimeout( ()=>saludando(anton, 'che')(),1000)
+
+saludar.call(lis,'Hola wey') //parametros separados por coma
+
+saludar.apply(anton, ['Hola amigo']) //parametros dentro de un arreglo separados por coma
